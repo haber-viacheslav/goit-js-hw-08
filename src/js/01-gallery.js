ccee13dc3dcc405be5ad336afe94c1f2fc91f0e8
Haber Viacheslav <gaberslavik@gmail.com>
2022-11-03 09:06:25 +0200

    added changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryRef: document.querySelector('.gallery'),
+};
+
+function addgaleryItemsMarkup(itemsArr) {
+  return itemsArr
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+    })
+    .join('');
+}
+
+refs.galleryRef.insertAdjacentHTML(
+  'beforeend',
+  addgaleryItemsMarkup(galleryItems)
+);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  loop: true,
+  captionDelay: 300,
+  animationSpeed: 300,
+  animationSlide: true,
+});
